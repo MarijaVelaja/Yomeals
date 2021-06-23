23bc8b97c1e80574b2b656875f55afc6aaa1178c
MarijaVelaja <marijavelaja@gmail.com>
2021-06-24 00:06:55 +0200

    waiter updated

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasicPage {
 
-	public LoginPage(WebDriver driver, JavascriptExecutor js) {
-		super(driver, js);
+	public LoginPage(WebDriver driver, JavascriptExecutor js,WebDriverWait waiter) {
+		super(driver, js, waiter);
 	
 	}
 	
