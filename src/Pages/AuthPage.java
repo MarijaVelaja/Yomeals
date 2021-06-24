404b170cf5aaf7d0b5d611c8a48cb762e9b9bbf5
MarijaVelaja <marijavelaja@gmail.com>
2021-06-24 23:24:20 +0200

    AuthPage

---
+++ src/Pages/AuthPage.java
@@ -0,0 +1,37 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+
+	}
+
+	public WebElement GetAccountDropDown() {
+		return this.driver.findElement(By.xpath("//div[@class='accounts-link accounts-popup']/ul/li/a"));
+	}
+
+	public WebElement getMyAccount() {
+		return this.driver.findElement(By.xpath("//div[@class='my-account-dropdown']/ul/li/a"));
+	}
+
+	public WebElement getLogOut() {
+		return this.driver.findElement(By.xpath("//div[@class='my-account-dropdown']/ul/li[2]/a"));
+	}
+
+	public void logOut() {
+		this.GetAccountDropDown().click();
+		waiter.until(ExpectedConditions.visibilityOf(getLogOut()));
+		this.getLogOut().click();
+	}
+	
+	
+	
+}
