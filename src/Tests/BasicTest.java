56b37bff4190d38e84b76a370ef1246b6d9bb9f8
MarijaVelaja <marijavelaja@gmail.com>
2021-06-25 14:36:36 +0200

    BasicTestModified

--- src/Tests/BasicTest.java
+++ src/Tests/BasicTest.java
@@ -1,8 +1,13 @@
 package Tests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import Pages.AuthPage;
 import Pages.CartSummaryPage;
@@ -14,8 +19,6 @@ import Pages.ProfilePage;
 
 public abstract class BasicTest {
 
-	
-	
 	protected WebDriver driver;
 	protected JavascriptExecutor js;
 	protected WebDriverWait wait;
@@ -25,22 +28,39 @@ public abstract class BasicTest {
 	protected ProfilePage profilePage;
 	protected AuthPage authPage;
 	protected MealPage mealPage;
-	protected CartSummaryPage cartSummaryPage;
+	protected CartSummaryPage cartPage;
 	protected String baseURL = "http://demo.yo-meals.com/";
 	protected String email = "customer@dummyid.com";
 	protected String password = "12345678a";
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@BeforeMethod
+
+	public void setup() {
+
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.js = (JavascriptExecutor) driver;
+		this.wait = new WebDriverWait(driver, 10);
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+		loginPage = new LoginPage(driver, js, wait);
+		popUpPage = new LocationPopupPage(driver, js, wait);
+		notificationPage = new NotificationSistemPage(driver, js, wait);
+		profilePage = new ProfilePage(driver, js, wait);
+		authPage = new AuthPage(driver, js, wait);
+		mealPage = new MealPage(driver, js, wait);
+		cartPage = new CartSummaryPage(driver, js, wait);
+
+	}
+
+	@AfterMethod
+
+	public void cleanup() {
+		this.driver.quit();
+	}
 	
 	
 	
