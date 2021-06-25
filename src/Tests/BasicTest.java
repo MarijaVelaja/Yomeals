02b12ffbf66fa9820f35c243349941e2b5f8c5e0
MarijaVelaja <marijavelaja@gmail.com>
2021-06-25 14:27:59 +0200

    BasicTest

--- src/Tests/BasicTest.java
+++ src/Tests/BasicTest.java
@@ -1,15 +1,62 @@
 package Tests;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import Pages.AuthPage;
+import Pages.CartSummaryPage;
+import Pages.LocationPopupPage;
+import Pages.LoginPage;
+import Pages.MealPage;
+import Pages.NotificationSistemPage;
+import Pages.ProfilePage;
+
 public abstract class BasicTest {
 
-	protected String username;
-	protected String password;
 	
 	
-	public BasicTest (String username,String password) {
-		this.username=username;
-		this.password=password;
-	}
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait wait;
+	protected LoginPage loginPage;
+	protected LocationPopupPage popUpPage;
+	protected NotificationSistemPage notificationPage;
+	protected ProfilePage profilePage;
+	protected AuthPage authPage;
+	protected MealPage mealPage;
+	protected CartSummaryPage cartSummaryPage;
+	protected String baseURL = "http://demo.yo-meals.com/";
+	protected String email = "customer@dummyid.com";
+	protected String password = "12345678a";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 	
